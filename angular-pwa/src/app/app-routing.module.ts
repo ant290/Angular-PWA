83e71bb9af3581b70c35a8eb3ebc283aca5d7101
ant290 <ant290@live.co.uk>
2020-08-06 16:59:07 +0100

    first commit

---
+++ angular-pwa/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ArticlesTechnologyComponent } from './articles-technology/articles-technology.component';
+import { ArticlesJavascriptComponent } from './articles-javascript/articles-javascript.component';
+
+
+const routes: Routes = [
+  { path: 'articles', component: ArticlesTechnologyComponent },
+  { path: 'articles-javascript', component: ArticlesJavascriptComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ angular-pwa/src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { HttpClientModule } from '@angular/common/http';
+import { NewsapiService } from './services/newsapi.service';
+import { ArticlesTechnologyComponent } from './articles-technology/articles-technology.component';
+import { ArticlesJavascriptComponent } from './articles-javascript/articles-javascript.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    ArticlesTechnologyComponent,
+    ArticlesJavascriptComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
+  ],
+  providers: [NewsapiService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
